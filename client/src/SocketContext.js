e4261a99c40e53ef10467f87745491682d3c371f
Uday Srivastava <udayomsrivastava111@gmail.com>
2024-06-26 15:07:11 +0530

    Update SocketContext.js Render deployment backend

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -5,7 +5,7 @@ import Peer from "simple-peer";
 const SocketContext = createContext();
 
 // const socket = io("http://localhost:5000");
-const socket = io("https://video-chat-production-4b07.up.railway.app/");
+const socket = io("https://video-chat-k8x7.onrender.com/");
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null);
